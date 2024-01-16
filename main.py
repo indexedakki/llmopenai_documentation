25b32c7631f013f9b452da844fb025a4826af057
indexedakki <74480055+indexedakki@users.noreply.github.com>
2024-01-16 18:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 
 import openai
- 
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+
 openai.api_type = 'azure'
 openai.api_key = 'b59f23e204b3426c9dbe1f6741b80acb'
 openai.api_version = "2022-12-01"
